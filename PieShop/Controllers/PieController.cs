a64a5d4b718a3c94fb104683171245525ef0b431
Helen Hua <helenhwl@163.com>
2022-09-26 15:42:11 +0800

    initial commit

---
+++ PieShop/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using PieShop.Models;
+using PieShop.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly Models.IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(Models.IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+
+            //return View(_pieRepository.AllPies);
+            PieListViewModel piesListViewModel = new PieListViewModel();
+            piesListViewModel.Pies = (System.Collections.Generic.IEnumerable<Pie>)_pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ PieShop/ViewModel/PieListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PieShop.Models;
+using System.Collections.Generic;
+
+namespace PieShop.ViewModel
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
